ffda4ec880049af773b64a2e144ebc7599681f8c
cedric <cedric.chok@gmail.com>
2020-10-30 19:04:14 +0100

    add cat model + cat services

--- src/app/models/Cat.ts
+++ src/app/models/Cat.ts
@@ -1,6 +1,33 @@
 export class Cat {
   id: number;
   name: string;
+  age: number;
   race: string;
   isAlive: boolean;
+
+  // Getters & Setters
+
+  // public getName() {
+  //   return this.name;
+  // }
+  //
+  // public setName(nom: string): void {
+  //   this.name = nom;
+  // }
+  //
+  // public getRace() {
+  //   return this.race;
+  // }
+  //
+  // public setRace(race: string): void {
+  //   this.race = race;
+  // }
+  //
+  // public GetIsAlive() {
+  //   return this.isAlive;
+  // }
+  //
+  // public setIsAlive(enVie: boolean): void {
+  //   this.isAlive = enVie;
+  // }
 }
---
+++ src/app/services/cat.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {Cat} from '../models/Cat';
+import {AppConfig, HOST} from '../../../config/app.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatService {
+
+  constructor(private http: HttpClient) { }
+
+  private url: string = HOST.apiUrl;
+
+  getAll(): Observable<Cat[]> {
+    return this.http.get<Cat[]>(this.url);
+  }
+
+  getById(id: number): Observable<Cat> {
+    return this.http.get<Cat>(this.url + id);
+  }
+
+  create(cat: Cat): Observable<Cat> {
+    return this.http.post<Cat>(this.url, cat);
+  }
+
+  update(cat: Cat): Observable<Cat> {
+    return this.http.patch<Cat>(this.url, cat);
+  }
+
+  delete(id: number): Promise<any> {
+    return fetch(this.url + '/' + id, {
+      method: 'DELETE'
+    });
+  }
+}
